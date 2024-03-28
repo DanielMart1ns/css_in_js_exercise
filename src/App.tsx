48f295586450ff82a7af88d4d89cf188213e4232
DanielMart1ns <danielmmrodrigues09@gmail.com>
2024-03-28 17:04:37 -0300

    feat: styled components created to Conteiner and Global

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,16 @@ import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
 import './global.css'
+import { Container } from './styles'
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
+      </Container>
     </>
   )
 }
